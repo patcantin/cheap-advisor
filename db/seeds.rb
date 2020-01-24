6a5363fe642027ae7a4b1bb466397416b2500f72
patcantin <patcantin23@gmail.com>
2020-01-24 15:16:03 -0500

    cheap-advisor complete crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "lafleur", address: "2020 st-denis", rating: 1)
+Restaurant.create(name: "lutetia", address: "de la montagne", rating: 4)
+Restaurant.create(name: "renoir", address: "sherbrooke ouest", rating: 5)
+Restaurant.create(name: "dominos", address: "fleury", rating: 1)
+Restaurant.create(name: "bistrot", address: "sauve", rating: 2)
